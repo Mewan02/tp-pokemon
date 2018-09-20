165d8fb3460f0a81cbd2d624d119b6a92ddb3e26
Mewan02 <mewandu75@gmail.com>
2018-09-20 13:35:44 +0200

    tp

---
+++ PokemonNetbean/src/server/Database.java
@@ -0,0 +1,96 @@
+package server;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import shared.Pokemon;
+
+/**
+ * This class represents the server database. In this project, it will simply
+ * provides an API for the server to interact with the file system.
+ *
+ * @author strift
+ *
+ */
+public class Database {
+
+    /**
+     * The name of the file used to store the data
+     */
+    private File file;
+
+    /**
+     * Constructor
+     *
+     * @param fileName the name of the file used to store the data
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public Database(String fileName) {
+        this.file = new File(fileName);
+        System.out.println("In Database");
+    }
+
+    /**
+     * Load the list of Pokemons stored inside the file and returns it
+     *
+     * @return
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    @SuppressWarnings("unchecked")
+    public ArrayList<Pokemon> loadPokemons() throws IOException, ClassNotFoundException {
+        ArrayList<Pokemon> data = new ArrayList<Pokemon>();
+
+        // This checks if the file actually exists
+        if (this.file.exists() && !this.file.isDirectory()) {
+            /*
+             * TODO
+             * Classes you can use:
+             * - ArrayList<Pokemon>
+             * - FileInputStream
+             * - ObjectInputStream
+             */
+            FileInputStream fis = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            data = (ArrayList<Pokemon>) ois.readObject();
+            ois.close();
+            fis.close();
+
+        } else {
+            System.out.println("Le fichier de sauvegarde n'existe pas.");
+        }
+
+        System.out.println(data.size() + " Pokémon(s) chargé(s) depuis la sauvegarde.");
+        return data;
+    }
+
+    /**
+     * Save the list of Pokémons received in parameters
+     *
+     * @param data the list of Pokémons
+     * @throws IOException
+     */
+    public void savePokemons(ArrayList<Pokemon> data) throws IOException {
+        /*
+         * TODO
+         * Classes you can use:
+         * - FileOutputStream
+         * - ObjectOutputStream
+         */
+        FileOutputStream fos = new FileOutputStream(file);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(new Pokemon("Pikachu2", 50));
+        oos.close();
+        fos.close();
+
+        System.out.println("Sauvegarde effectuée... " + data.size() + " Pokémon(s) en banque.");
+    }
+}
